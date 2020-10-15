112f295aeb1b6b0abb1e153c1e6f9a25c943c2fd
embbnux <embbnux@embbnux.com>
2020-10-15 13:31:15 +0800

    clean code

--- src/run-server.js
+++ src/run-server.js
@@ -2,15 +2,12 @@
 require('dotenv').config()
 
 const { server } = require('./server');
-// import initDb from './app/common/init-db';
 
 const {
   APP_PORT: port,
   APP_HOST: host,
-  APP_HOME = '/'
 } = process.env;
 
 server.listen(port, host, () => {
-  console.log(`-> server running at: http://${host}:${port}${APP_HOME}`);
-  // initDb();
+  console.log(`-> server running at: http://${host}:${port}`);
 });
