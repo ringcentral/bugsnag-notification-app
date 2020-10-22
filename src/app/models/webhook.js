54bad52114084d1b41260af2192caca904839fc2
embbnux <embbnux@embbnux.com>
2020-10-22 21:33:58 +0800

    fix: issue in lambda

--- src/app/models/webhook.js
+++ src/app/models/webhook.js
@@ -1,11 +1,12 @@
 const Sequelize = require('sequelize');
+const { generate } = require('shortid');
 const { sequelize } = require('./sequelize');
 
 exports.Webhook = sequelize.define('webhooks', {
   id: {
     type: Sequelize.STRING,
     primaryKey: true,
-    defaultValue: Sequelize.UUIDV4
+    defaultValue: generate,
   },
   rc_webhook: {
     type: Sequelize.STRING
