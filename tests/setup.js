50a90af56e6761a259618e925cec456fad97e99a
Embbnux Ji <embbnux.ji@ringcentral.com>
2021-09-10 07:26:39 +0000

    tests: add tests for notify API

--- tests/setup.js
+++ tests/setup.js
@@ -2,6 +2,8 @@ const { Webhook } = require('../src/app/models/webhook');
 const { AuthToken } = require('../src/app/models/authToken');
 const { RCWebhook } = require('../src/app/models/rc-webhook');
 
+jest.setTimeout(30000);
+
 beforeAll(async () => {
   await Webhook.sync();
   await AuthToken.sync();
