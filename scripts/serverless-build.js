12af04ceee831ebe2f4c4f88e4625f1efae8372f
Embbnux Ji <embbnux@embbnux.com>
2021-12-21 11:23:14 +0800

    fix: CI build issue

--- scripts/serverless-build.js
+++ scripts/serverless-build.js
@@ -7,10 +7,10 @@ const deployPath = resolve(projectPath, 'serverless-deploy')
 echo('clean path...');
 rm('-rf', `${deployPath}/*.js`);
 rm('-rf', `${deployPath}/*.json`);
-rm('-rf', `${deployPath}/app`);
+rm('-rf', `${deployPath}/server`);
 rm('-rf', `${deployPath}/node_modules`);
 echo('building...');
-cp('-r', `${projectPath}/src/app`, `${deployPath}/app`);
+cp('-r', `${projectPath}/src/server`, `${deployPath}/server`);
 cp(`${projectPath}/src/server.js`, `${deployPath}/server.js`);
 cp(`${projectPath}/src/lambda.js`, `${deployPath}/lambda.js`);
 cp(`${projectPath}/package.json`, `${deployPath}/package.json`);
