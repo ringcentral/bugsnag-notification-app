8926ee58fc23031b8f01b94195a59f9fa18e8c3c
Embbnux Ji <embbnux@embbnux.com>
2022-02-09 16:27:19 +0800

    feat: support to redirect bugsnag message to bot

--- src/server/routes/notification.js
+++ src/server/routes/notification.js
@@ -1,11 +1,13 @@
 const crypto = require('crypto');
+const axios = require('axios');
 const Bot = require('ringcentral-chatbot-core/dist/models/bot').default;
 
-const { requestWithoutWaitingResponse } = require('../utils/requestWithoutWaitingResponse');
+const { ICON_URL } = require('../utils/constants');
+const { decodeToken } = require('../utils/jwt');
 const { Webhook } = require('../models/webhook');
 const { notificationInteractiveMessages, botInteractiveMessagesHandler } = require('../handlers/interactiveMessages');
 
-const { formatAdaptiveCardMessage } = require('../utils/formatAdaptiveCardMessage');
+const { formatBugsnagMessageIntoCard } = require('../utils/formatAdaptiveCardMessage');
 
 // notification from Bugsnag for notification add-in
 async function notification(req, res) {
@@ -17,19 +19,38 @@ async function notification(req, res) {
     return;
   }
   // console.log(JSON.stringify(req.body, null, 2));
-  const message = formatAdaptiveCardMessage(req.body, id);
+  const card = formatBugsnagMessageIntoCard({
+    bugsnagMessage: req.body,
+    webhookId: id,
+    messageType: 'Notification',
+  });
   // console.log(JSON.stringify(message.attachments[0], null, 2));
   // request without waiting response to reduce lambda function time
-  await requestWithoutWaitingResponse(webhookRecord.rc_webhook, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(message),
-  });
-  res.status(200);
-  res.send('ok');
+  let body;
+  if (card) {
+    body = { attachments: [card] };
+  } else {
+    const project = req.body.project;
+    body = {
+      title: `**${req.body.trigger && req.body.trigger.message}** for [${project && project.name}](${project && project.url})`,
+    };
+  }
+  body.icon = ICON_URL;
+  body.activity = 'Bugsnag Add-in';
+  try {
+    await axios.post(webhookRecord.rc_webhook, body, {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    });
+    res.status(200);
+    res.send('ok');
+  } catch (e) {
+    console.error(e);
+    res.status(500);
+    res.send('error');
+  }
 };
 
 // notification from Bugsnag for bot add-in
@@ -43,13 +64,17 @@ async function botNotification(req, res) {
   }
   const { botId, groupId } = decodedToken;
   // console.log(JSON.stringify(req.body, null, 2));
-  const message = formatAdaptiveCardMessage(req.body, id);
+  const card = formatBugsnagMessageIntoCard({
+    bugsnagMessage: req.body,
+    messageType: 'Bot',
+    botId,
+  });
   // console.log(JSON.stringify(message.attachments[0], null, 2));
   // request without waiting response to reduce lambda function time
   try {
     const bot = await Bot.findByPk(botId);
     if (bot) {
-      await bot.sendAdaptiveCard(groupId, message);
+      await bot.sendAdaptiveCard(groupId, card);
     }
     res.status(200);
     res.send('ok');
