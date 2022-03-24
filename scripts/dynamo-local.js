08dcfa591e26471eae2d497f577f8e0b0bafca05
Embbnux Ji <embbnux@embbnux.com>
2022-03-24 17:49:19 +0800

    chore: dynamo local

--- scripts/dynamo-local.js
+++ scripts/dynamo-local.js
@@ -1,13 +1,21 @@
+const path = require('path');
+var fs = require('fs');
 const DynamoDbLocal = require('dynamodb-local');
 
-const dynamoLocalPort = 8000;
+const dynamoLocalPort = 8000
 
-async function init () {
+const dbPath = path.resolve(__dirname, '../.dynamodb');
+
+if (!fs.existsSync(dbPath)){
+  fs.mkdirSync(dbPath);
+}
+
+async function init() {
   console.log('Starting dynamodb server, please wait, do not quit');
   await DynamoDbLocal.launch(
-    dynamoLocalPort, null, [], false, true
+    dynamoLocalPort, dbPath, ['-sharedDb'], false, false
   );
-  console.log('local dynamdb started at port:', dynamoLocalPort);
+  console.log('local dynamodb started at port:', dynamoLocalPort);
 }
 
 init();
