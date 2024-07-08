6e31207eb401165d9b3ae17f3a1135d77950608c
Embbnux Ji <embbnux.ji@ringcentral.com>
2024-07-08 13:52:47 +0800

    misc: add referer checker and csp header (#72)

---
+++ src/server/utils/refererChecker.js
@@ -0,0 +1,35 @@
+function getOrigin(uri) {
+  if (!uri) {
+    return null;
+  }
+  const url = new URL(uri);
+  return url.origin;
+}
+
+const KNOWN_REFERER_HOSTS = [
+  getOrigin(process.env.APP_SERVER),
+  getOrigin(process.env.RINGCENTRAL_CHATBOT_SERVER),
+];
+
+function refererChecker(req, res, next) {
+  const referrer = req.get('Referer');
+  if (!referrer) {
+    res.status(403).send('No Referer');
+    return;
+  }
+  const referrerOrigin = getOrigin(referrer);
+  if (
+    KNOWN_REFERER_HOSTS.find(host => {
+      if (!host) {
+        return false;
+      }
+      return host === referrerOrigin;
+    })
+  ) {
+    next();
+    return;
+  }
+  res.status(403).send('Invalid Referer');
+};
+
+exports.refererChecker = refererChecker;
